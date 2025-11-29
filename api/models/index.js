ff1d207e1d19e4d810b5fd8e6d9c2bb73b18644d
miniotti <robin.miniotti@etu.unistra.fr>
2025-11-29 17:29:01 +0100

    update

--- api/models/index.js
+++ api/models/index.js
@@ -1,16 +1,26 @@
 const { Sequelize } = require('sequelize');
 const { DATABASE_URL, DB_SSL } = require('../config');
 
+// Prefer a full DATABASE_URL but fall back to the assembled URL from config.js
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: DB_SSL ? {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    }
-  } : {},
+  dialectOptions: {
+    // If DB_SSL is truthy enable ssl. Some environments (local) may not need it.
+    ssl: DB_SSL,
+    native: true,
+  },
   define: {
     timestamps: false,
   },
 });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.pollution = require("./pollution.model.js")(sequelize, Sequelize);
+db.utilisateur = require('./utilisateur.model.js')(sequelize, Sequelize);
+
+module.exports = db;
