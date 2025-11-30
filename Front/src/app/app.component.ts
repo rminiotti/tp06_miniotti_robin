746e5fae952df67a95a8b00c8c5bce72da285026
miniotti <robin.miniotti@etu.unistra.fr>
2025-11-30 12:24:36 +0100

    update

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -5,10 +5,14 @@ import { PollutionDetailsComponent } from "./pollution-details/pollution-details
 import { PollutionListComponent } from "./pollution-list/pollution-list.component";
 import { UserModule } from './user/user.module';
 import { UserServiceService } from './user-service.service';
+import { Store } from '@ngxs/store';
+import { FavorisState } from './favoris/favoris.state';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, PollutionDetailsComponent, PollutionListComponent, UserModule],
+  imports: [RouterOutlet, RouterLink, PollutionDetailsComponent, PollutionListComponent, UserModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true
@@ -16,8 +20,11 @@ import { UserServiceService } from './user-service.service';
 export class AppComponent implements OnInit {
   title = 'tp06';
   loggedIn = true;
+  favorisCount$: Observable<number>;
 
-  constructor(private router: Router, private userService: UserServiceService) {}
+  constructor(private router: Router, private userService: UserServiceService, private store: Store) {
+    this.favorisCount$ = this.store.select(FavorisState.getFavorisCount);
+  }
   
   ngOnInit(): void {
     this.userService.isLoggedIn$.subscribe(isLoggedIn => {
